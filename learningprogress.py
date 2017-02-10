919e63cd79f5940cf68d3de2a1cc0572e334d67c
PaulUpendo <lovepaul46@outlook.com>
2017-02-10 15:45:07 +0300

    2nd Commit

--- learningprogress.py
+++ learningprogress.py
@@ -4,6 +4,7 @@ class Skills(object):
     def __init__(self):
         self.date_posted = str(datetime.datetime.now().date())
         self.skills = []
+        self.studied = []
 
     def add_skill(self):
         skill_name = str(input('Type the skill title: '))
@@ -11,8 +12,15 @@ class Skills(object):
         self.skills.append(skill_name)
         return self.skills
 
-    def find_missing(self, studied):
+    def add_studied(self):
+        skill_name = str(input('Type the skill title: '))
+        self.studied.append(skill_name)
+
+       # return self.studied
+
+    def unstudied(self):
         skills = self.skills
+        studied = self.studied
         missing = []
         for skill in studied:
             if skill not in skills:
